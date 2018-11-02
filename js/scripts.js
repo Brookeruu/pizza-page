294ff9c2176f107283458026f891dc1cfa185cbb
Brooke Perry <perrysbrooke@gmail.com>
2018-11-02 09:12:30 -0700

    add pizza and order constructors to js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function OrderTotal() {
+this.pizzas = [];
+}
+
+OrderTotal.prototype.addPizzaToOrder = function() {
+this.pizzas.push(pizza);
+}
+
+function Pizza (cheese,toppings,size) {
+  this.cheese = cheese;
+  this.toppings = toppings;
+  this.size = size;
+}
+
+Pizza.prototype.
+
+Pizza.prototype.pizzaCost = function() {
+
+}
+
+
+var pizza = new Pizza();
+orderTotal.addPizzaToOrder(pizza);
